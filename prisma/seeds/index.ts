6d24e41c08de1e2b1b78532645bd44780c85163e
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-05 02:39:21 -0400

    Update index.ts

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   try {
-    const user_status = await prisma.user_Status.createMany({
+    const user_status = await prisma.user_status.createMany({
       data: [
         { name: "Pending Validation" },
         { name: "Active" },
@@ -33,9 +33,22 @@ async function main() {
       skipDuplicates: true,
     });
 
-    const token_types = await prisma.token_type
+    const token_types = await prisma.token_type.createMany({
+      data: [{ name: "loggin" }, { name: "verification" }, { name: "reset password" }],
+      skipDuplicates: true,
+    });
 
-    console.log({ documet_types, user_status, booking_status });
+    const provider_types = await prisma.provider_type.createMany({
+      data: [{ name: "Google" }, { name: "Facebook" }],
+      skipDuplicates: true,
+    });
+
+    const user_types = await prisma.user_type.createMany({
+      data: [{ name: "Barber" }, { name: "Customer" }],
+      skipDuplicates: true,
+    });
+
+    console.log({ documet_types, user_status, booking_status, token_types, provider_types, user_types });
   } catch (e) {
     console.error(e);
   }
