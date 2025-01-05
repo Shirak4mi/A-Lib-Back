dfd351736bbae52c407aa78dc29db5a5c5b8778e
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-05 05:51:24 -0400

    +adds

--- prisma/migrations/20250105090240_add/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "TemporalTokens_hashed_token_key";
-
--- DropIndex
-DROP INDEX "TemporalTokens_user_id_key";
-
--- AlterTable
-ALTER TABLE "TemporalTokens" ALTER COLUMN "expires_at" SET DATA TYPE DATE;
