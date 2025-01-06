b049be2a7d1e5426e0b8e0e85f60011d30b85f4b
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-06 11:54:00 -0400

    Update Get.ts

--- src/controllers/Bussiness/methods/Get.ts
+++ src/controllers/Bussiness/methods/Get.ts
@@ -1,5 +1,6 @@
-import { prisma } from "@/db/index.ts";
+import { NotFoundException } from "@/utils/error/index.ts";
 import { FilterBussinessDTO } from "../dtos/index.ts";
+import { prisma } from "@/db/index.ts";
 
 import { Elysia } from "elysia";
 
@@ -11,16 +12,31 @@ export default new Elysia().get(
       const { address, bussiness_type, description, email, name, open_from, open_to, owner_id, phone_number } = query;
 
       const filteredBussiness = await prisma.business.findMany({
+        select: {
+          name: true,
+          email: true,
+          address: true,
+          description: true,
+          phone_number: true,
+          open_to: true,
+          open_from: true,
+          Owner: { select: { email: true } },
+          Bussiness_Type: { select: { name: true } },
+        },
         where: {
           name: { equals: name },
           email: { equals: email },
           address: { equals: address },
+          Owner: { id: { equals: owner_id } },
           description: { equals: description },
           phone_number: { equals: phone_number },
+          Bussiness_Type: { id: { equals: bussiness_type } },
         },
       });
 
-      console.log({ filteredBussiness });
+      if (!filteredBussiness) throw new NotFoundException("Theres no bussiness under this params");
+
+      return filteredBussiness;
     } catch (e) {
       throw e;
     }
