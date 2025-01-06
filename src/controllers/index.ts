30963810bfcad24e22f0ff1b849f55d5f7fdd3b0
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-06 11:06:30 -0400

    +adds

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,9 @@
 import Bussiness from "./Bussiness/index.ts";
+import Bookins from "./Bookins/index.ts";
 import Profile from "./Profile/index.ts";
+import Queue from "./Queue/index.ts";
 import Auth from "./Auth/index.ts";
 
 import { Elysia } from "elysia";
 
-export const routes = new Elysia({ prefix: "api" }).use(Auth).use(Profile).use(Bussiness);
+export const routes = new Elysia({ prefix: "api" }).use(Auth).use(Profile).use(Bussiness).use(Queue).use(Bookins);
