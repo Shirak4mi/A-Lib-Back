8cacdae79214994e5d546698d30ea0159cdf6f69
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-07 01:29:29 -0400

    Update Find.ts

--- src/controllers/Bussiness/methods/Find.ts
+++ src/controllers/Bussiness/methods/Find.ts
@@ -1,4 +1,6 @@
+import { InternalServerErrorException } from "@/utils/error";
 import { IdBasedTokenDTO } from "@/common/DTO";
+import { prisma } from "@/db";
 
 import { Elysia } from "elysia";
 
@@ -6,6 +8,24 @@ export default new Elysia().get(
   "Find/:id",
   async ({ params: { id } }) => {
     try {
+      const business = await prisma.business.findFirst({
+        select: {
+          name: true,
+          email: true,
+          address: true,
+          description: true,
+          phone_number: true,
+          open_to: true,
+          open_from: true,
+          Owner: { select: { email: true } },
+          Bussiness_Type: { select: { name: true } },
+        },
+        where: { id },
+      });
+
+      if (!business) throw new InternalServerErrorException("Cannot Find Business with this id!");
+
+      return business;
     } catch (e) {
       throw e;
     }
