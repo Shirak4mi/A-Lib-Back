3d8db13b24002bf4f0c8eebbe2d89838c5d476fd
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-07 02:06:01 -0400

    +adds

--- src/controllers/Queue/index.ts
+++ src/controllers/Queue/index.ts
@@ -3,10 +3,12 @@ import DeleteQueuePosition from "./methods/DeleteQueuePosition.ts";
 import UpdateQueuePosition from "./methods/UpdateQueuePosition.ts";
 import FinishQueuePosition from "./methods/FinishQueuePosition.ts";
 import GetQueuePositions from "./methods/GetQueuePositions.ts";
+import GetByBusiness from "./methods/GetByBusiness.ts";
 
 import { Elysia } from "elysia";
 
 export default new Elysia({ prefix: "Queue", normalize: true, detail: { tags: ["Queue"] } })
+  .use(GetByBusiness)
   .use(GetQueuePositions)
   .use(CreateQueuePosition)
   .use(DeleteQueuePosition)
