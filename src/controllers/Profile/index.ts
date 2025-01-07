63870e36dd51de17e0ba99ae39166cc672a65934
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-07 02:13:07 -0400

    +adds

--- src/controllers/Profile/index.ts
+++ src/controllers/Profile/index.ts
@@ -1,3 +1,10 @@
+import GetBookins from "./methods/GetBookins.ts";
+import Update from "./methods/Update.ts";
+import GetMe from "./methods/GetMe.ts";
+
 import { Elysia } from "elysia";
 
-export default new Elysia({ prefix: "Profile", normalize: true, detail: { tags: ["Profile"] } });
+export default new Elysia({ prefix: "Profile", normalize: true, detail: { tags: ["Profile"] } })
+  .use(GetMe)
+  .use(Update)
+  .use(GetBookins);
