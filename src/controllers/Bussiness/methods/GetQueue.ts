1fe4242b401df2c3ed2cc91bf0b4837cdb1e29d6
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-07 01:40:30 -0400

    +adds

--- src/controllers/Bussiness/methods/GetQueue.ts
+++ src/controllers/Bussiness/methods/GetQueue.ts
@@ -1,5 +1,6 @@
 import { IdBasedTokenDTO } from "@/common/DTO";
 import { prisma } from "@/db";
+import { InternalServerErrorException } from "@/utils/error";
 
 import { Elysia } from "elysia";
 
@@ -7,14 +8,14 @@ export default new Elysia().get(
   "GetQueue/:id",
   async ({ params: { id } }) => {
     try {
-      
       const businessQueues = await prisma.business.findMany({
         select: { Queue: { select: { id: true } } },
         where: { id },
       });
 
+      if (!businessQueues) throw new InternalServerErrorException();
 
-
+      return businessQueues;
     } catch (e) {
       throw e;
     }
--- src/controllers/Bussiness/methods/Update.ts
+++ src/controllers/Bussiness/methods/Update.ts
@@ -1,5 +1,7 @@
+import { InternalServerErrorException } from "@/utils/error";
 import { IdBasedTokenDTO } from "@/common/DTO";
 import { UpdateBussinessDTO } from "../dtos";
+import { prisma } from "@/db";
 
 import { Elysia } from "elysia";
 
@@ -7,11 +9,25 @@ export default new Elysia().patch(
   "Update/:id",
   async ({ params: { id }, body }) => {
     try {
+      const updatedBussiness = await prisma.business.update({
+        where: { id },
+        select: {
+          name: true,
+          email: true,
+          address: true,
+          description: true,
+          phone_number: true,
+          open_to: true,
+          open_from: true,
+          Owner: { select: { email: true } },
+          Bussiness_Type: { select: { name: true } },
+        },
+        data: {},
+      });
 
+      if (!updatedBussiness) throw new InternalServerErrorException();
 
-
-
-      
+      return updatedBussiness;
     } catch (e) {
       throw e;
     }
