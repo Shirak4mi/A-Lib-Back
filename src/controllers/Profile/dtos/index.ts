0d863c39f0d35663d76eece801c117751984cce2
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-08 00:57:52 -0400

    +adds

--- src/controllers/Profile/dtos/index.ts
+++ src/controllers/Profile/dtos/index.ts
@@ -1,1 +1,7 @@
 import { t, type Static } from "elysia";
+
+export const GetBookinsQueryDTO = t.Object({
+  page: t.Number({ default: 1 }),
+});
+
+export type TGetBookinsQueryDTO = Static<typeof GetBookinsQueryDTO>;
--- src/controllers/Profile/methods/GetBookins.ts
+++ src/controllers/Profile/methods/GetBookins.ts
@@ -1,3 +1,38 @@
+import { UnauthorizedException } from "@/utils/error";
+import { AddMetaData, getCurrentCursorOffSet } from "@/utils/functions";
+import { sessionCookie } from "@/common/DTO";
+import { GetBookinsQueryDTO } from "../dtos";
+import { prisma } from "@/db";
+
 import { Elysia } from "elysia";
 
-export default new Elysia().get("Bookins", async () => {});
+export default new Elysia().get(
+  "Bookins",
+  async ({ cookie: { session }, query }) => {
+    try {
+      const validSession = await prisma.session.findUnique({
+        select: { expires_at: true, user_id: true },
+        where: { id: session.value },
+      });
+
+      if (!validSession) throw new UnauthorizedException("The requested session does not exists");
+
+      const { page } = query;
+
+      const [data, totalRegisters] = await prisma.$transaction([
+        prisma.booking.findMany({
+          where: { Booked_By: {} },
+          take: 50,
+          orderBy: { id: "asc" },
+          skip: getCurrentCursorOffSet(page),
+        }),
+        prisma.booking.count({ where: { Booked_By: {} } }),
+      ]);
+
+      return AddMetaData(data, page, totalRegisters);
+    } catch (e) {
+      throw e;
+    }
+  },
+  { cookie: sessionCookie, query: GetBookinsQueryDTO }
+);
