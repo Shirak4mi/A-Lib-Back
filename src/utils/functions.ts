0607f3186c639502fbc94191e4a0e42aad9c694f
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-08 00:28:01 -0400

    +adds

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -53,17 +53,18 @@ export function getRandomHash(): string {
 
 /**
  * 
- * {
-  "address": "calle X uwehuiwfe",
-  "birth_date": "27/09/1999",
-  "first_name": "Jose",
-  "last_name": "Ynfante",
-  "phone_number": "8099919999",
-  "document_type_id": 1,
-  "account_type_id": 1,
-  "document_id": "402372991999",
-  "password": "admin1234",
-  "email": "yassett.ynfante@gmail.com"
-}
+ * 
+  {
+    "address": "calle X uwehuiwfe",
+    "birth_date": "27/09/1999",
+    "first_name": "Jose",
+    "last_name": "Ynfante",
+    "phone_number": "8099919999",
+    "document_type_id": 1,
+    "account_type_id": 1,
+    "document_id": "402372991999",
+    "password": "admin1234",
+    "email": "yassett.ynfante@gmail.com"
+  }
  * 
  */
