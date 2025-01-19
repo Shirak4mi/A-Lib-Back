5722e581205c06116ce8301469a592cfc7f785f5
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-19 01:12:35 -0400

    +adds

--- src/controllers/Profile/dtos/index.ts
+++ src/controllers/Profile/dtos/index.ts
@@ -5,3 +5,18 @@ export const GetBookinsQueryDTO = t.Object({
 });
 
 export type TGetBookinsQueryDTO = Static<typeof GetBookinsQueryDTO>;
+
+export const UpdateProfileDTO = t.Object({
+  first_name: t.Optional(t.String({ minLength: 1 })),
+  last_name: t.Optional(t.String({ minLength: 1 })),
+  email: t.Optional(t.String({ minLength: 1, format: "email" })),
+  username: t.Optional(t.String({ minLength: 1 })),
+  password: t.Optional(t.String({ minLength: 8 })),
+  birth_date: t.Optional(t.String()),
+  phone_number: t.Optional(t.String()),
+  document_id: t.Optional(t.String()),
+  document_type_id: t.Optional(t.Union([t.String(), t.Number()])),
+  profile_picture: t.Optional(t.File({ maxItems: 1, type: "image/png" })),
+});
+
+export type TUpdateProfileDTO = Static<typeof UpdateProfileDTO>;
--- src/controllers/Profile/methods/Update.ts
+++ src/controllers/Profile/methods/Update.ts
@@ -1,3 +1,33 @@
+import { saveUserProfilePicture } from "@/utils/file_management";
+import { session_cookie_name } from "@/utils/constants";
+import { generateRandomSalt } from "@/utils/functions";
+import { UnauthorizedException } from "@/utils/error";
+import { sessionCookie } from "@/common/DTO";
+import { prisma } from "@/db";
+
 import { Elysia } from "elysia";
 
-export default new Elysia().patch("Update", async () => {});
+export default new Elysia().patch(
+  "Update",
+  async ({ cookie, body }) => {
+    try {
+      const id = cookie[session_cookie_name].value;
+      const isValidSession = await prisma.session.findFirst({
+        select: { User: { select: { id: true, username: true } } },
+        where: { id },
+      });
+
+      if (!isValidSession) throw new UnauthorizedException("Not Authorized!");
+
+      // Sugary Destructs
+      const { User } = isValidSession;
+      const { birth_date, document_type_id, password } = body;
+
+      const isFileSaved = await saveUserProfilePicture(body.profile_picture, `public/${User.username}`);
+      const password_salt = generateRandomSalt();
+    } catch (e) {
+      throw e;
+    }
+  },
+  { cookie: sessionCookie, body: UpdateProfileDTO }
+);
