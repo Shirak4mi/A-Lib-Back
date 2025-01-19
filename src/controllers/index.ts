42dcf9903b02721730c48d618f188a73b7312643
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-19 01:01:17 -0400

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
+import { useErrorMiddleware, useSuccessResponseMiddleware } from "@/middlewares/response.ts";
 import Bussiness from "./Business/index.ts";
 import Bookins from "./Bookins/index.ts";
 import Profile from "./Profile/index.ts";
@@ -6,4 +7,7 @@ import Auth from "./Auth/index.ts";
 
 import { Elysia } from "elysia";
 
-export const routes = new Elysia({ prefix: "api" }).use([Auth, Profile, Bussiness, Queue, Bookins]);
+export const routes = new Elysia({ prefix: "api" })
+  .use(useSuccessResponseMiddleware)
+  .use(useErrorMiddleware)
+  .use([Auth, Profile, Bussiness, Queue, Bookins]);
