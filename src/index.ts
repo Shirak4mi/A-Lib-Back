71abd3512b0eba45f1701700599ae1f1effe91a0
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-01-19 00:41:00 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Elysia } from "elysia";
 
 const helmetExec = helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: { policy: "same-site" } });
 
-new Elysia({ name: "Renter API", strictPath: true, precompile: true })
+new Elysia({ name: "E-Tickets API", strictPath: true, precompile: true })
   .use(swagger({ theme: "Dark", version: "0.1", exclude: ["/swagger", "/swagger/json"] }))
   .get("ping", () => "pong", { tags: ["Test"] })
   .use(helmetExec)
