0fdd222c616f2c2002703183b4fac0761fc70e47
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-12 14:46:01 -0400

    Update VerifyLinks.ts

--- src/controllers/Auth/methods/VerifyLinks.ts
+++ src/controllers/Auth/methods/VerifyLinks.ts
@@ -1,4 +1,6 @@
-import { BadRequestException } from "@/utils/error";
+import { createUserSessions, invalidateUserSessions } from "@/utils/session";
+import { BadRequestException, ImATeapotException } from "@/utils/error";
+import { session_cookie_name } from "@/utils/constants";
 import { CommonTokenDTO } from "@/common/DTO";
 import { prisma } from "@/db";
 
@@ -6,21 +8,54 @@ import { Elysia } from "elysia";
 
 export default new Elysia().post(
   "VerifyLink/:token",
-  async ({ params: { token: short_code } }) => {
+  async ({ params: { token: short_code }, cookie, set, redirect }) => {
     try {
       const isValidNano = await prisma.temporal_short_links.findFirst({
         where: { short_code },
         select: {
-          expires_at: true,
-          original_url: true,
+          User: { select: { email: true, id: true } },
           Type: { select: { id: true } },
-          User: { select: { email: true } },
+          original_url: true,
+          expires_at: true,
         },
       });
       if (!isValidNano) throw new BadRequestException("The Provided Token is not a valid One");
+      // Sugar Destructuration for better accesibility => Type existance already validated
+      const { expires_at, original_url, Type, User } = isValidNano;
 
-      console.log({ isValidNano });
-      
+      // Check Expiration
+
+      switch (Type.id) {
+        case 1:
+          // Sessions Cleaning
+          const cleanSessions = await invalidateUserSessions(Type.id);
+          if (!cleanSessions) return;
+
+          // New Session Creating
+          const session = await createUserSessions(Type.id);
+          if (!session) return;
+
+          // New Cookie Creation
+          cookie[session_cookie_name].set({
+            secure: Bun.env.NODE_ENV !== "production",
+            maxAge: session.expires_at,
+            value: session.id,
+            sameSite: "lax",
+            httpOnly: true,
+            path: "/",
+          });
+
+          // Set Response Status => 201 -> A new Session Was Created
+          set.status = 201;
+
+          return redirect("http://localhost:3000/en/Home", 302);
+        case 2:
+          break;
+        case 3:
+          break;
+        default:
+          throw new ImATeapotException("Looks like this impossible case happened, I'm indeed a teapot");
+      }
     } catch (e) {
       console.log(e);
       throw e;
